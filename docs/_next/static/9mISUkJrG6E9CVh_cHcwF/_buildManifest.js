0c90a085d650cdd0155d050c3141128f14df8f6e
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-10 17:51:19 +0700

    initial

---
+++ docs/_next/static/9mISUkJrG6E9CVh_cHcwF/_buildManifest.js
@@ -0,0 +1,11 @@
+self.__BUILD_MANIFEST = {
+	'/': ['static/chunks/2a0bc6d22434da4d.js'],
+	'/_error': ['static/chunks/2fdc7ae2cbc8a7a3.js'],
+	__rewrites: {
+		afterFiles: [],
+		beforeFiles: [],
+		fallback: [],
+	},
+	sortedPages: ['/', '/_app', '/_error'],
+};
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+const BASE_PATH = 'words';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  reactStrictMode: true,
+	/* config options here */
+	trailingSlash: true,
+	reactCompiler: true,
+	reactStrictMode: true,
+	basePath: NODE_ENV === 'development' ? '' : `/${BASE_PATH}`,
+	output: NODE_ENV === 'development' ? 'standalone' : 'export',
+	distDir: NODE_ENV === 'development' ? '.next' : 'docs',
 };
 
 export default nextConfig;
